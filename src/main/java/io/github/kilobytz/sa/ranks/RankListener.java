6284ef451148804babf7e9458c8542ea6d6510d0
Kilobytz <j.m.williams@hotmail.co.nz>
2020-10-25 01:01:53 +1300

    more fixes

--- src/main/java/io/github/kilobytz/sa/ranks/RankListener.java
+++ src/main/java/io/github/kilobytz/sa/ranks/RankListener.java
@@ -1,38 +1,30 @@
 package io.github.kilobytz.sa.ranks;
 
 
-import com.mojang.authlib.GameProfile;
 import io.github.kilobytz.sa.SA;
-import io.github.kilobytz.sa.misc.Reflection;
 import io.netty.channel.Channel;
 import net.minecraft.server.v1_12_R1.PacketPlayOutPlayerInfo;
 import org.bukkit.Bukkit;
 import org.bukkit.GameMode;
-import org.bukkit.Location;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
-import org.bukkit.event.block.BlockBreakEvent;
 import org.bukkit.event.player.PlayerJoinEvent;
 import org.bukkit.event.player.PlayerLoginEvent;
-import org.bukkit.scheduler.BukkitScheduler;
 import io.github.kilobytz.sa.misc.TinyProtocol;
-
-import java.lang.reflect.Field;
 
 public class RankListener implements Listener {
 
     RankManager rM;
     SA main;
     private TinyProtocol protocol;
-    private Class<?> playerInfo = Reflection.getClass("{nms}.PacketPlayOutPlayerInfo");
-    private Class<Object> playerData = Reflection.getUntypedClass("{nms}.PacketPlayOutPlayerInfo.PlayerInfoData");
-    private Reflection.FieldAccessor<Object> playerInfoData = Reflection.getField(playerInfo,playerData,0);
+
+
 
     public void setRanks(RankManager rM, SA main) {
         this.rM = rM;
         this.main = main;
-        //packetListen();
+        packetListen();
     }
 
     @EventHandler
@@ -46,7 +38,6 @@ public class RankListener implements Listener {
             if (rM.doesPlayerHaveRank(event.getPlayer())) {
                 String rank = (String) main.getConfig().get("users." + event.getPlayer().getUniqueId().toString());
                 if (rank.equalsIgnoreCase("builder")) {
-                    rM.removeRanks(event.getPlayer());
                     rM.builder(event.getPlayer());
                     rM.setTitle(event.getPlayer(), "builder");
                     return;
@@ -70,14 +61,13 @@ public class RankListener implements Listener {
             event.disallow(PlayerLoginEvent.Result.KICK_OTHER,"Server is still starting! Please wait before reconnecting.");
         }
     }
-/*
+
     public void packetListen() {
         protocol = new TinyProtocol(main) {
             @Override
             public Object onPacketOutAsync(Player receiver, Channel channel, Object packet) {
-                if(playerInfo.isInstance(packet)) {
-                    //PacketPlayOutPlayerInfo.PlayerInfoData data = (PacketPlayOutPlayerInfo.PlayerInfoData) playerInfoData.get(packet);
-                    receiver.sendMessage(data.toString());
+                if(packet instanceof PacketPlayOutPlayerInfo) {
+
                 }
                 return super.onPacketOutAsync(receiver, channel, packet);
             }
